766fbf109a78410892021d3aaffe21bcc957bbdf
Roman Schuster <schuster@sas.upenn.edu>
2017-01-14 14:50:33 -0500

    Fixed redundant recording message
    * fixed redundant recording message
    * reformatted comments

--- smartscale.sh
+++ smartscale.sh
@@ -12,7 +12,6 @@ export GCLOUD_PROJECT="smartscale-rs"
 ##### Recording Audio #####
 ###########################
 
-
 # Checking if speech.wav already exists
 CURRENTDIR=$(pwd)
 CHECK4WAV=$(find $CURRENTDIR -name speech.wav | wc -l)
@@ -22,18 +21,15 @@ if [ $CHECK4WAV == 1 ]; then
     sudo rm -rf speech.wav
 fi
 
-
 # Recording audio file
-	# Filename			:	'speech.wav'
-	# Sample Rate		:	16000 (16khz)
-	# Precision			:	Signed 16-bit Little Endian Integer PCM
-	# Duration			:	00:00:10.00 = 16000 samples ~ 750 CDDA sectors
-	# File Size			:	650kb
-	# Bit Rate			:	512k
-	# VU-meter			:	mono (1 channel)
-echo "Recording"
+	# Filename	:	'speech.wav'
+	# Sample Rate	:	16000 (16khz)
+	# Precision	:	Signed 16-bit Little Endian Integer PCM
+	# Duration	:	00:00:10.00 = 16000 samples ~ 750 CDDA sectors
+	# File Size	:	650kb
+	# Bit Rate	:	512k
+	# VU-meter	:	mono (1 channel)
 arecord -f cd -r 16000 -d 10 -t wav -c 1 -D plughw:1,0 speech.wav
-
 
 # Making sure the reording saved properly
 CHECK4WAV=$(find $CURRENTDIR -name speech.wav | wc -l)
@@ -44,10 +40,8 @@ else
 	echo "Recording Not Saved"
 fi
 
-
 #################################
 ##### Calling Python Script #####
 #################################
-
 
 python speech.py speech.wav
