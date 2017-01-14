0ed89b78da3a85b699b6076ebc3a7e7ecfba9c7c
Roman Schuster <schuster@sas.upenn.edu>
2017-01-14 14:22:46 -0500

    Added main shell script

---
+++ smartscale.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# SmartScale main shell script
+
+########################################
+##### Google Environment Variables #####
+########################################
+
+export GOOGLE_APPLICATION_CREDENTIALS="/home/pi/SmartScale/SmartScale-b53168c05da1.json"
+export GCLOUD_PROJECT="smartscale-rs"
+
+###########################
+##### Recording Audio #####
+###########################
+
+
+# Checking if speech.wav already exists
+CURRENTDIR=$(pwd)
+CHECK4WAV=$(find $CURRENTDIR -name speech.wav | wc -l)
+
+if [ $CHECK4WAV == 1 ]; then
+    echo "Removing old WAV file"
+    sudo rm -rf speech.wav
+fi
+
+
+# Recording audio file
+	# Filename			:	'speech.wav'
+	# Sample Rate		:	16000 (16khz)
+	# Precision			:	Signed 16-bit Little Endian Integer PCM
+	# Duration			:	00:00:10.00 = 16000 samples ~ 750 CDDA sectors
+	# File Size			:	650kb
+	# Bit Rate			:	512k
+	# VU-meter			:	stereo
+echo "Recording"
+arecord -f cd -r 16000 -d 10 -t wav -D plughw:1,0 speech.wav
+
+
+# Making sure the reording saved properly
+CHECK4WAV=$(find $CURRENTDIR -name speech.wav | wc -l)
+
+if [ $CHECK4WAV == 1 ]; then
+	echo "Recording Successfully Saved"
+else
+	echo "Recording Not Saved"
+fi
+
+
+#################################
+##### Calling Python Script #####
+#################################
+
+
+python speech.py speech.wav
