c9323656978ae0a35e36837372a8fdc080b54bf6
Roman Schuster <schuster@sas.upenn.edu>
2017-01-24 19:12:29 -0500

    Completely integrated a (hopefully) functional translation method

--- lcd.py
+++ lcd.py
@@ -35,6 +35,7 @@ def get_speech_service():
     return discovery.build(
         'speech', 'v1beta1', http=http, discoveryServiceUrl=DISCOVERY_URL)
 
+  
 def format_string_for_lcd(lcd_columns, lcd_rows, text_string):
     '''
     Returns a list of strings to print on a lcd_columns x lcd_rows display
@@ -59,7 +60,8 @@ def format_string_for_lcd(lcd_columns, lcd_rows, text_string):
 
     return list_of_strings
 
-def translate_text(txt = 'Enter some text', from_lang = 'en', to_lang = 'fr'):
+  
+def translate_text(txt = 'Please enter some text', from_lang = 'en', to_lang = 'fr'):
     '''
     Runs authentication for Microsoft Translate service on Azure Marketplace
     Args:
@@ -69,24 +71,30 @@ def translate_text(txt = 'Enter some text', from_lang = 'en', to_lang = 'fr'):
     Returns:
         string containing the translation of txt from from_lang to to_lang
     '''
+    # Authenticating
     auth_key = '7d2c5a67c24c45ce90fc219a3fae8bf9'
     auth_url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken'
-    auth_headers = { 'Ocp-Apim-Subscription-Key' : auth_key }
+    auth_headers = {'Ocp-Apim-Subscription-Key' : auth_key}
     auth_response = requests.post(auth_url, headers = auth_headers)
     token = auth_response.text
-    
+
+    # Translating
     translate_url = 'https://api.microsofttranslator.com/v2/http.svc/Translate'
-    translate_params = { 'appid' : ('Bearer ' + token), 'text' : txt, 'from' : from_lang, 'to' : to_lang }
-    translate_headers = { 'Accept' : 'application/xml' }
+    translate_params = { 'appid' : ('Bearer ' + token), 'text' : txt, 'from' : from_lang, 'to' : to_lang}
+    translate_headers = {'Accept' : 'application/xml'}
     translate_response = requests.get(translate_url, params = translate_params, headers = translate_headers)
     
-    return translate_response.text
+    # Ooooooweeee translate_response.txt has some weird ass HTML formatting to clean up
+    # There are 68 useless chars at the beginning and 9 at the end to get rid of
+    FIRST_REAL_CHAR = 68
+    LAST_REAL_CHAR = len(translate_response.text) - 9
+    return translate_response.text[FIRST_REAL_CHAR:LAST_REAL_CHAR]
 
 
     
 def main(speech_file):
     '''
-    Transcribe the given audio file and prints it on the lcd
+    Transcribe the given audio file, translates it, and prints it out
 
     Args:
         speech_file: the name of the audio file.
@@ -137,8 +145,10 @@ def main(speech_file):
 
     # Initialize the LCD using the pins above.
     lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+    
+    translated_string = translate_text(result_string)
 
-    messages = format_string_for_lcd(lcd_columns, lcd_rows, result_string)
+    messages = format_string_for_lcd(lcd_columns, lcd_rows, translated_string)
     for msg in messages:
         lcd.clear()
         lcd.message(msg)
