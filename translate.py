a2e7f57150d5b5bcd5e7bab874a3e185182b6046
Roman Schuster <schuster@sas.upenn.edu>
2017-01-25 13:35:50 -0500

    Much better formatting algorithm (requires 2 rows, any number of columns)

--- translate.py
+++ translate.py
@@ -17,3 +17,50 @@ def translate_text(txt = 'Please enter some text', from_lang = 'en', to_lang = '
     translate_headers = {'Accept' : 'application/xml'}
     translate_response = requests.get(translate_url, params = translate_params, headers = translate_headers)
     return translate_response.text
+
+def lcd_msg_formatter(txt, cols):
+    '''
+    Formats a string into a list of strings to be printed out on the LCD
+    Note that for now the LCD MUST have 2 rows
+    Any number of columns
+    Args:
+        txt: string
+        cols: int
+    Returns:
+        messages: list of strings perfectly formatted for an Adafruit LCD
+    '''
+    messages = []
+    txt_copy = ''
+    for i in txt:
+        txt_copy += i
+
+    msg = ''
+    while len(txt_copy) > 0:
+        if len(txt_copy) < cols:
+            msg += txt_copy
+            messages += [msg]
+            break
+            
+        lastCharIdx = cols
+        for j in range(cols + 1):
+            if txt[(cols + 1) - j] == ' ' or txt[(cols + 1) - j] == '-':
+                lastCharIdx = ((cols + 1) - j)
+                break
+                
+        if len(msg) == 0:
+            for i in range(lastCharIdx):
+                msg += txt_copy[i]
+            msg += '\n'
+            txt_copy = txt_copy[(lastCharIdx - 1):]
+        else:
+            for i in range(lastCharIdx):
+                msg += txt_copy[i]
+            messages += [msg]
+            msg = ''
+            txt_copy = txt_copy[(lastCharIdx - 1):]
+            
+    return messages
+        
+        
+                
+        
